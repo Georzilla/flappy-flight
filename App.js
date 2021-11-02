a96daf5eed037bf1966493eb25b3ab26635fc11a
Jorge Rondón <jfrrj19@gmail.com>
2021-11-01 21:26:53 -0500

    flappy bird, obstacles stage

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import Bird from './components/Bird';
+
 
 export default function App() {
+  const screenWidth = Dimensions.get('screen').width
+  const screenHeight = Dimensions.get('screen').height
+  const birdLeft = screenWidth / 2
+  const [birdBottom, setBirdBottom] = useState(screenHeight/2)
+  
+  const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth)
+  
+  const gravity = 3
+  let gameTimerId
+  let obstaclesLeftTimerId
+
+/// start falling
+
+useEffect(() => {
+  if (birdBottom > 0){
+    gameTimerId = setInterval(() => {
+      setBirdBottom(birdBottom => birdBottom - gravity)
+    }, 30)
+
+    return () => {
+      clearInterval(gameTimerId)
+    }
+
+
+  } 
+}, [birdBottom])
+console.log(birdBottom)
+
+//start obstacles
+useEffect(() => {
+  if(obstaclesLeft > 0) {
+    obstaclesLeftTimerId = setInterval(() => {
+      setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
+    }, 30)
+  }
+  
+  return () => {
+    clearInterval(obstaclesLeftTimerId)
+  }
+
+}, [obstaclesLeft])
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Bird
+        birdBottom={birdBottom}
+        birdLeft={birdLeft}
+     
+     />
     </View>
+      
+
+    
   );
 }
 
---
+++ components/Bird.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, ViewBase, ViewComponent } from 'react-native';
+
+
+const Bird = ({birdBottom, birdLeft}) => {
+    const birdWidth = 50
+    const birdHeight = 50
+
+    return (
+
+    <View style= {{
+
+            position: 'absolute',
+            backgroundColor: 'teal',
+            width: birdWidth,
+            height: birdHeight,
+            borderRadius: 50,
+            left: birdLeft - (birdWidth/2),
+            bottom: birdBottom - (birdHeight/2),
+        }} />
+        )
+    
+}
+
+
+export default Bird
+
+
